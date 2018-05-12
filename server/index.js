5a10dd1fa07f15e5a4c7ff6d982024d7b178ba99
michellelockett <shelldog42@gmail.com>
2018-05-12 12:14:13 -0700

    remove unnecessary server junk:

--- server/index.js
+++ server/index.js
@@ -1,17 +1,12 @@
 const db = require('../db/models/index.js');
-
-const bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
 const { dailyFetch } = require('../helpers/events');
 
-const wrap = fn => (...args) => fn(...args).catch(args[2]);
+console.log(process.env);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 //TODO cron job to fetch every 24 hours
 
 //TODO cron job to check DB for newly relevant events
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
